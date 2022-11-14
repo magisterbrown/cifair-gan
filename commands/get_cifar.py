817f01eebf482b9202810da9ad71b0a490a2eaed
Artur <magisterbrownie@gmail.com>
2022-11-14 13:57:38 +0100

    cloner command

---
+++ commands/get_cifar.py
@@ -0,0 +1,22 @@
+from .base_command import BaseCommand
+import os
+import json
+
+class GetCifar(BaseCommand):
+    def __init__(self,inputs):
+        self.add_arg('o','out','path to save ds')
+        super().__init__(inputs)
+
+        self.path = self.pathify(self.args.out)
+
+    def submit(self):
+        os.system(f'gsutil cp gs://monet-cool-gan/cifar10gn.tar {self.path}')
+        os.system(f'tar -xf {self.path}cifar10gn.tar -C {self.path}')
+        json_pth=f'{self.path}config_cifar10.json'
+        with open(json_pth, "r+") as f:
+            json_repr = json.load(f)
+            json_repr['pathDB'] = f'{self.path}cifar10_res/'
+            f.write(json.dumps(json_repr))
+
+
+
---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+class Boss:
+    def __init__(self):
+        self.els = dict()
+
+    def add_command(self, key: str, func: callable):
+        self.els[key] = func
+
+    def get_command(self, key):
+        return self.els[key]()
+
+def get_cifar():
+        from commands.get_cifar import GetCifar as comandor
+        return comandor
+
+if __name__=='__main__':
+    boss = Boss()
+    boss.add_command('get_cifar', get_cifar)
+    
+    try:
+        subcommand = sys.argv[1]
+
+        Comador = boss.get_command(subcommand) 
+        task = Comador(sys.argv[2:])
+        task.submit()
+    except IndexError:
+        print('Options: ')
+        print(list(boss.els.keys()))  # Display help if no arguments were given.
+
+
+    
+
+
+
