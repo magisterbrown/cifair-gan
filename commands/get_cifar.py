7de7271d07d9f156ede73edfc270add85f442660
Artur <magisterbrownie@gmail.com>
2022-11-14 16:27:40 +0000

    not json change

--- commands/get_cifar.py
+++ commands/get_cifar.py
@@ -13,10 +13,10 @@ class GetCifar(BaseCommand):
         os.system(f'gsutil cp gs://monet-cool-gan/cifar10gn.tar {self.path}')
         os.system(f'tar -xf {self.path}cifar10gn.tar -C {self.path}')
         json_pth=f'{self.path}config_cifar10.json'
-        with open(json_pth, "r+") as f:
-            json_repr = json.load(f)
-            json_repr['pathDB'] = f'{self.path}cifar10_res/'
-            f.write(json.dumps(json_repr))
+        #with open(json_pth, "r+") as f:
+        #    json_repr = json.load(f)
+        #    json_repr['pathDB'] = f'{self.path}cifar10_res/'
+        #    f.write(json.dumps(json_repr))
 
 
 
