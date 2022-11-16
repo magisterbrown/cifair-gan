6cfaf1574fee26be3dd6c691509bb61f56839cdb
Artur <magisterbrownie@gmail.com>
2022-11-16 23:49:15 +0000

    Starting processes

--- call.sh
+++ call.sh
@@ -1,2 +1,2 @@
 python3 main.py cif_to_tar -i data/cifar10_res/ -o data/tfolds #Create tfolds for it
-python3 submodules/pytorch_GAN_zoo_xla/train.py XLAPGAN -c config_cifar10.json #Train xla pgan
+python3 submodules/pytorch_GAN_zoo_xla/train.py XLAPGAN -c config_cifar10.json -p 60 #Train xla pgan
